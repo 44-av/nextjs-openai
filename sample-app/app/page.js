4efbf2b8e1541faadfa976fa700812d60c9f3080
Ariane Villalino <arianevillalino@gmail.com>
2024-08-08 21:03:00 +0800

    error component

--- sample-app/app/page.js
+++ sample-app/app/page.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about">To About</Link>
+      <h1 className="text-5xl mb-8 font-bold">Test</h1>
+      <Link href="/about" className="btn btn-accent">
+        Get Started
+      </Link>
     </div>
   );
 };
